a7776775e43d59694ef5f9ff35947658d010f3bb
yashy16 <54349328+yashy16@users.noreply.github.com>
2025-12-30 14:28:46 +0530

    script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 
-directory_to_backup="/home/root5/DirectoryToBackup" # this is the directory we want to zip and create a backup 
+directory_to_backup="/home/root5/DirectoryToBackup" # this is the directory we want to zip and create a backup. This is the hardcore if we want to take path from usr we will write "$1".
 
-backup_location="/home/root5/"	# This is the path we want to store the Backup
+backup_location="/home/root5/"	# This is the path we want to store the Backup, If we want to take location from user we will write instead of path "$2".
 
 current_date=$(date +%Y-%m-%d) 	#Value of the current date which will be appended in the backup file name
 
